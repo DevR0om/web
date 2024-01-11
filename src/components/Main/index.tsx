2382befa5d518337f06197f0fc70062bd0b84ba8
Dandara Bianca <dandarabiancas@gmail.com>
2024-01-11 18:38:31 -0300

    Feat(23): Added main button

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import styles from "./styles.module.scss";
+import MainButton from '../UI/MainButton';
 
-interface MainProps{
+interface MainProps {
     lang: any;
-} 
+}
 
 export default function Main({ lang }: MainProps) {
     return (
         <div className={styles.main_container}>
+            <div className={styles.buttonCard}>
+            <MainButton 
+            color='black' 
+            fontSize='16px' 
+            text={lang.buttonMainText} 
+            fontWeight='bold'
+            />
+            </div>
         </div>
     );
 }
