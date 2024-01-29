a2653fe27cb2fc953bffafa6aea00f39981d2047
unknown <clovissantannaa@gmail.com>
2024-01-29 16:43:23 -0300

    Fix

--- src/utils/pageError.ts
+++
@@ -1,9 +0,0 @@
-async function pageError(url: string): Promise<boolean> {
-    try{
-        const response = await fetch(url)
-        return response.status === 404;
-    }catch(err){
-        return false
-    }
-}
-export default pageError;
