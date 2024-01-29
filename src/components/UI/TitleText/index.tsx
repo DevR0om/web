64fc9e03447477831a4bfbeeb8c5e7843a75bccf
unknown <clovissantannaa@gmail.com>
2024-01-29 16:48:24 -0300

    Fix

---
+++ src/components/UI/TitleText/index.tsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.scss';
+
+interface TitleProps {
+    colorText: string;
+    fontSize: string;
+    text:string;
+}
+
+export default function TitleText({ colorText, fontSize, text }: TitleProps) {
+
+    return (
+        <h1 className={styles.TitleText} style={{ color: colorText, fontSize, fontFamily: 'sans-Serif' }}>
+            {text}
+        </h1>
+    );
+}
